3c3bcc71c3f6b0ec07666c969893fe80038936d9
Mohammad Hunan Chughtai <mohammadhunan@gmail.com>
2019-06-04 00:11:11 -0400

    Create loops_logging.js

---
+++ loops_logging.js
@@ -0,0 +1,16 @@
+/*
+
+  CREATE A PROGRAM WHICH READS AN ARRAY OF SOCIAL MEDIA POSTS AND LOGS IT TO THE CONSOLE 
+
+*/
+
+
+var posts = [
+  "today is friday, I like fridays",
+  "game of thrones finale was horrible",
+  "game of thrones finale was a satisfying end"
+]
+
+for(var index = 0; index < posts.length; index++){
+  console.log(posts[index]);
+}
