48a110ffb1662bfc09048e453a90b07c5c15f457
Mohammad Hunan Chughtai <mohammadhunan@gmail.com>
2019-06-04 00:19:32 -0400

    Create decrement_loop.js

---
+++ decrement_loop.js
@@ -0,0 +1,9 @@
+/*
+
+CREATE A PROGRAM WHICH DECREMENTS A COUNTER AND PRINTS ITS VALUE
+
+*/
+
+for(let time = 5; time > 0; time--){
+  console.log("you have " + time + " minutes left in the test")
+}
